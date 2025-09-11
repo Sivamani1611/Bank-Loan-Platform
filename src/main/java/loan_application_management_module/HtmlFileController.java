96140c64a779cfc4493855bedc2173a5ccd83490
Sanjeeva <2429946@cognizant.com>
2025-09-11 09:18:47 +0530

    Added Loan Application WebPage

---
+++ src/main/java/loan_application_management_module/HtmlFileController.java
@@ -0,0 +1,14 @@
+package loan_application_management_module;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HtmlFileController {
+
+    @GetMapping
+    public ModelAndView showLoanAppPage() {
+        return new ModelAndView("loanAppModule");
+    }
+}
