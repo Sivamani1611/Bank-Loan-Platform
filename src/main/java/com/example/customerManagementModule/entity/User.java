81ed3bcec0dc1e2c96d680a4a0d3147e7fa28968
Sanjeeva <2429946@cognizant.com>
2025-09-11 10:07:33 +0530

    Added Admin/Customer Validation.

--- src/main/java/com/example/customerManagementModule/entity/User.java
+++ src/main/java/com/example/customerManagementModule/entity/User.java
@@ -16,6 +16,8 @@ public class User {
 
     private String password;
 
+    private String role;
+
     private Integer customerId;
 
     public Integer getUserId() {
@@ -44,6 +46,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public Integer getCustomerId() {
